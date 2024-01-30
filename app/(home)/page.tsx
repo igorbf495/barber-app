0d6768ec164c27dc71dbade5d888dd5343c4711b
igorbf495 <igorcarvalhofelipe495@gmail.com.br>
2024-01-29 22:40:02 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,24 @@
 import { format, formatDate } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-     <div className="px-5 pt-5">
-     <h2 className="text-xl font-bold">Olá, Igor!</h2>
-      <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
-        locale: ptBR
-      })}
-      </p>
-     </div>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Igor!</h2>
+        <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+          locale: ptBR
+        })}
+        </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
     </div>
   );
 }
