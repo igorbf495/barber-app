97f8bfb245df0143a90a0a4bf402560894f74ca4
igorbf495 <igorcarvalhofelipe495@gmail.com.br>
2024-01-29 22:16:38 -0300

    feat: add welcome mensage

---
+++ app/(home)/page.tsx
@@ -0,0 +1,19 @@
+import { format, formatDate } from "date-fns";
+import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+
+     <div className="px-5 pt-5">
+     <h2 className="text-xl font-bold">Olá, Igor!</h2>
+      <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+        locale: ptBR
+      })}
+      </p>
+     </div>
+    </div>
+  );
+}
